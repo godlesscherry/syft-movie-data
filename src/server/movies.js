9b44f6dce53b96ed8843a4bfbd13974d8a47c5a0
Hari Charan <haricharan.nitt@gmail.com>
2024-12-27 00:56:39 +0530

    feat: normalize movie data from IMDB and RT providers

--- src/server/movies.js
+++ src/server/movies.js
@@ -8,14 +8,16 @@ import { getMovies as getRT } from "./providers/rt";
 export async function getRecentMovies() {
     // Recent movies only shows the top 5 movies from both providers.
     // so "Breaking bad" will not be shown in the recent movies. This is not a bug.
-    const movies = [...await getImdb(), ...await getRT()]
+    const movies = [...await getImdb(), ...await getRT()];
     return movies.slice(0, 5);
 }
 
 export async function getGenereAndMovies() {
     const movies = [...await getImdb(), ...await getRT()];
     const genres = movies.reduce((acc, movie) => {
-        const genres = movie.genre?.split(",").map((genre) => genre.trim());
+        const genreField = movie.genre || movie.Genre;
+        console.log(genreField , movie.title);
+        const genres = genreField?.split(",").map((genre) => genre.trim());
         genres?.forEach((genre) => {
             if (!acc[genre]) {
                 acc[genre] = [];
--- src/server/providers/imdb/index.js
+++ src/server/providers/imdb/index.js
@@ -3,5 +3,31 @@
 import data from "./data.json";
 
 export async function getMovies() {
-    return data.movies;
+    return data.movies.map(normalizeImdbMovie)
+}
+function normalizeImdbMovie(movie) {
+    return {
+        title: movie.title || movie.Title,
+        year: movie.year || movie.Year,
+        rated: movie.rated || movie.Rated,
+        released: movie.released || movie.Released,
+        runtime: movie.runtime || movie.Runtime,
+        genre: movie.genre || movie.Genre,
+        director: movie.director || movie.Director,
+        writer: movie.writer || movie.Writer,
+        actors: movie.actors || movie.Actors,
+        plot: movie.plot || movie.Plot,
+        language: movie.language || movie.Language,
+        country: movie.country || movie.Country,
+        awards: movie.awards || movie.Awards,
+        poster: movie.poster ?? movie.Poster ?? (movie.images && movie.images[0]),
+        metascore: movie.metascore || movie.Metascore,
+        imdbRating: movie.imdbRating || movie.imdbRating,
+        imdbVotes: movie.imdbVotes || movie.imdbVotes,
+        imdbID: movie.imdbID || movie.imdbID,
+        type: movie.type || movie.Type,
+        response: movie.response || movie.Response,
+        images: movie.images || movie.Images,
+        rtRating: movie.rtRating || movie.rtRating
+    };
 }
