bd3ca34fa8f5a0d5712b53c25f37a05852c7c5fe
Hari Charan <haricharan.nitt@gmail.com>
2024-12-27 00:50:22 +0530

    fix: add fallback image for movie poster on error

--- src/components/movie-card.js
+++ src/components/movie-card.js
@@ -1,23 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function MovieCard({ movie }) {
+    const [imgSrc, setImgSrc] = useState(movie.poster);
+
+    const handleImageError = () => {
+        setImgSrc('/images/fallback-image.png'); // Fallback image
+    };
+
     return (
         <div key={movie.id} className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
-        <Image
-            src={movie.poster}
-            alt={movie.title}
-            className="rounded-lg w-full h-64 object-cover"
-            width={300}
-            height={450}
-        />
-        <div className="mt-4">
-            <h2 className="text-md font-semibold text-gray-800 dark:text-white">
-                {movie.title}
-            </h2>
-            <p className="mt-2 text-gray-600 dark:text-gray-300">
-                {movie.plot}
-            </p>
-        </div>
-    </div>        
-    )
+            <Image
+                src={imgSrc}
+                alt={movie.title}
+                className="rounded-lg w-full h-64 object-cover"
+                width={300}
+                height={450}
+                onError={handleImageError}
+            />
+            <div className="mt-4">
+                <h2 className="text-md font-semibold text-gray-800 dark:text-white">
+                    {movie.title}
+                </h2>
+                <p className="mt-2 text-gray-600 dark:text-gray-300">
+                    {movie.plot}
+                </p>
+            </div>
+        </div>        
+    );
 }
